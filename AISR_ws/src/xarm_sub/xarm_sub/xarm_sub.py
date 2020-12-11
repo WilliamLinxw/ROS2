bdb198b2352d007711929c14e3d1cec9ab7338e2
CaesarLinxw <xiaowei.lin1997@gmail.com>
2020-12-11 16:22:09 +0800

    AISR_ws updated according to the current interface

--- AISR_ws/src/xarm_sub/xarm_sub/xarm_sub.py
+++ AISR_ws/src/xarm_sub/xarm_sub/xarm_sub.py
@@ -8,6 +8,7 @@ import sys
 import time
 import math
 import numpy as np
+import json
 
 from xarm.wrapper import XArmAPI
 
@@ -40,31 +41,124 @@ class WhiteArmSubscriber(Node):
         self.subscription = self.create_subscription(String, 'white_arm', self.listener_callback, 10)
         self.subscription  # prevent unused variable warning
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+    def move_one_servo_only(self, message):
 
-        # Get list for angles and speeds
-        angles_speeds= list(np.array(msg.data.split(" "), dtype=np.float))
+        '''
+        This function is now for moving one servo according to commands from Android
+        '''
 
-        # Extract angles
-        angles = angles_speeds[::2]
+        move_head_dict = json.loads(message)
+        servo_id_ROS = move_head_dict['id']
         
-        # Extract speeds
-        speeds = angles_speeds[1::2]
+        # ID offset
+        if servo_id_ROS == 3:
+            servo_id = 5
+        elif servo_id_ROS == 4:
+            servo_id = 6
+        elif servo_id_ROS == 5:
+            servo_id = 0
+        elif servo_id_ROS == 6:
+            servo_id = 1
+        elif servo_id_ROS == 7:
+            servo_id = 2
+        elif servo_id_ROS == 8:
+            servo_id = 3
+        elif servo_id_ROS == 9:
+            servo_id = 4
 
-        # Process the angle data
-        angles[1] = -angles[1]
-        angles[0] += math.degrees(-2.932)
-        angles[1] += math.degrees(1.187)
-        angles[2] += math.degrees(1.990)
-        angles[3] += math.degrees(1.012)
-        angles[4] += math.degrees(3.124)
+        current_servo_angle_xArm = arm.get_servo_angle(servo_id=servo_id)
 
-        # Select the speed and send the command
-        speed = min(max(speeds), 40)
+        # Angle offset
+        if servo_id == 6:
+            current_servo_angle = current_servo_angle_xArm + 54.5
+        elif servo_id == 7:
+            current_servo_angle = current_servo_angle_xArm - 26
+
+        servo_move_angle = move_head_dict['angle']
+        target_servo_angle = current_servo_angle + servo_move_angle
+        arm.set_servo_angle(servo_id=servo_id, angle=target_servo_angle, speed=10, wait=False)
+        print(arm.get_servo_angle(), arm.get_servo_angle(is_radian=True))
+
+
+    def move_any_servo(self, message):
+
+        '''
+        This function is for moving according to the result data of learning
+        '''
+
+        angles = [None, None, None, None, None, None, None]
+        message_list = list(np.array(message.split(" "), dtype=np.float))
+
+        servo_id = message_list[::3]
+        servo_angle = message_list[1::3]
+        servo_speed = message_list[2::3]
+        servo_id = list(map(int, servo_id))
+
+        # Put specific servo angle into the angle list
+        i_angle = 0
+        for id in servo_id:
+            angles[id] = servo_angle[i_angle]
+            i_angle += 1
+
+        # Process the angle datas for the offsets
+        i_servo = 0
+        while i_servo <= 6:
+            if angles[i_servo]:
+                if i_servo == 0:
+                    angles[i_servo] += -168
+                elif i_servo == 1:
+                    angles[i_servo] += 68
+                elif i_servo == 2:
+                    angles[i_servo] += 114
+                elif i_servo == 3:
+                    angles[i_servo] += 58
+                elif i_servo == 4:
+                    angles[i_servo] += 179
+                elif i_servo == 5:
+                    angles[i_servo] += 54.5
+                elif i_servo == 6:
+                    angles[i_servo] += -26
+            i_servo += 1
+
+        speed = min(max(servo_speed), 40)
         print('speed: ', speed)
         arm.set_servo_angle(angle=angles, speed=speed, is_radian=False, wait=False)
         print(arm.get_servo_angle(), arm.get_servo_angle(is_radian=True))
+
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+        # Determine whether to move the head only
+        if msg.data.find('id'):
+            print('Move one servo')
+            self.move_one_servo_only(msg.data)
+        else:
+            print('Move any servo')
+            self.move_any_servo(msg.data)
+
+        # # Get list for angles and speeds
+        # angles_speeds= list(np.array(msg.data.split(" "), dtype=np.float))
+
+        # # Extract angles
+        # angles = angles_speeds[::2]
+        
+        # # Extract speeds
+        # speeds = angles_speeds[1::2]
+
+        # # Process the angle data
+        # angles[1] = -angles[1]
+        # angles[0] += math.degrees(-2.932)
+        # angles[1] += math.degrees(1.187)
+        # angles[2] += math.degrees(1.990)
+        # angles[3] += math.degrees(1.012)
+        # angles[4] += math.degrees(3.124)
+
+        # # Select the speed and send the command
+        # speed = min(max(speeds), 40)
+        # print('speed: ', speed)
+        # arm.set_servo_angle(angle=angles, speed=speed, is_radian=False, wait=False)
+        # print(arm.get_servo_angle(), arm.get_servo_angle(is_radian=True))
 
 
 def main(args=None):
