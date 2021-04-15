c9e0a408e9a99f045a0a01e5754012c66458b104
CaesarLinxw <xiaowei.lin1997@gmail.com>
2021-04-15 17:04:06 +0800

    node for receiving Float64MultiArray realized

--- dev_ws/src/cpp_pubsub/src/subscriber_member_function.cpp
+++ dev_ws/src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -1,25 +1,31 @@
 #include <memory>
+#include <iostream>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
 
 using std::placeholders::_1;
+using namespace std;
 
 class MinimalSubscriber : public rclcpp::Node{
   public:
     MinimalSubscriber()
     : Node("minimal_subscriber")
     {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      subscription_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
       "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
     }
 
   private:
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    void topic_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg) const
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+      // RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+      for (uint i = 0; i < msg->data.size(); i++){
+        cout << msg->data[i] << endl;
+      }
     }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;};
+    rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr subscription_;};
 
 int main(int argc, char * argv[])
 {
